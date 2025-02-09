0ac907d513ce868b6b84bce91be7452587b55268
unknown <nikcevici13@gmail.com>
2025-02-09 17:14:35 +0100

    postcontroller

---
+++ app/Controllers/Admin/PostController.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace App\Controllers\Admin;
+
+
+use App\Models\Post;
+use App\Services\Auth;
+use App\Services\Authorization;
+use Core\Router;
+use Core\View;
+
+class PostController
+{
+  public function index()
+  {
+    return View::render(
+      template: 'admin/posts/index',
+      layout: 'layouts/admin',
+      data: [
+        'posts' => Post::all(),
+      ]
+    );
+  }
+
+
+  public function create()
+  {
+    return View::render(
+      template: 'admin/posts/create',
+      layout: 'layouts/admin'
+    );
+  }
+
+  public function store()
+  {
+    Authorization::verify('create_post');
+    $data = ['title' => $_POST['title'], 'content' => $_POST['content'], 'user_id' => Auth::user()->id];
+    Post::create($data);
+    Router::redirect('/admin/posts');
+  }
+
+  public function edit($id)
+  {
+    return View::render(
+      template: 'admin/posts/edit',
+      layout: 'layouts/admin',
+      data: [
+        'posts' => Post::find($id),
+      ]
+    );
+  }
+
+  public function update($id)
+  {
+    $post = Post::find($id);
+    Authorization::verify('edit_post', $post);
+    $post->title = $_POST['title'];
+    $post->content = $_POST['content'];
+    $post->save();
+    Router::redirect('/admin/posts');
+  }
+  public function delete($id)
+  {
+    $post = Post::find($id);
+    $post->delete();
+    Router::redirect('/admin/posts');
+  }
+}
